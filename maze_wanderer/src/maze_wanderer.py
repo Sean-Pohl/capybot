fc8cd9f9784652e963951b4abb6520597a7d618f
Sean-Pohl <seanpohl999@gmail.com>
2017-10-13 18:48:59 -0500

    It does shit

--- maze_wanderer/src/maze_wanderer.py
+++ maze_wanderer/src/maze_wanderer.py
@@ -1,12 +1,13 @@
 import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import LaserScan
+#Position 0 is back right
 
 dist = None
-llook = None
+lfeel = None
+rfeel = None
 rlook = None
-rcen = None
-lcen = None
+llook = None
 
 rospy.init_node("maze_wanderer")
 
@@ -19,14 +20,14 @@ def callback(data):
 	global dist
 	global llook
 	global rlook
-	global lcen
-	global rcen
+	global lfeel
+	global rfeel
 	mid = (len(data.ranges)/2)
 	ahead = data.ranges[mid]
-	lcen = data.ranges[mid-20]
-	rcen = data.ranges[mid+20]
-	llook = data.ranges[(len(data.ranges)/6)]
-	rlook = data.ranges[5*(len(data.ranges)/6)]
+	rlook = data.ranges[(len(data.ranges)/6)]
+	llook = data.ranges[5*(len(data.ranges)/6)]
+	rfeel = data.ranges[5*(len(data.ranges)/12)]
+	lfeel = data.ranges[7*(len(data.ranges)/12)]
 	dist = ahead
 	print(ahead)
 
@@ -37,35 +38,27 @@ def drive(left_speed, right_speed):
 	fr.publish(right_speed)
 	bl.publish(left_speed)
 	br.publish(right_speed)
-	
-def center():
-	if(lcen > rcen):
-		while(lcen-rcen > 0.0001):
-			drive(0.1,-0.1)
-			rospy.sleep(0.1)
-		drive(0,0)
-		rospy.sleep(0.25)
-	if(rcen > lcen):
-		while(rcen-lcen > 0.0001):
-			drive(-0.1,0.1)
-			rospy.sleep(0.1)
-		drive(0,0)
-		rospy.sleep(0.25)
 
 rospy.sleep(0.5)
 while(True):
-	while(dist > 0.25):
-		drive(5,5)
-		rospy.sleep(0.1)
-	drive(0,0)
-	rospy.sleep(0.5)
-	if(llook < rlook):
-		drive(-2.5,2.5)
-		rospy.sleep(2.0)
-		center()
-	else:
-		drive(2.5,-2.5)
-		rospy.sleep(2.0)
-		center()
-	drive(0,0)
-	rospy.sleep(0.5)
+	drive(5,5)
+	rospy.sleep(0.1)
+	if(rfeel < 0.35):
+		while(rfeel < 0.4):
+			drive(-2.5,2.5)
+			rospy.sleep(0.1)
+	if(lfeel < 0.35):
+		while(lfeel < 0.4):
+			drive(2.5,-2.5)
+			rospy.sleep(0.1)
+	if(dist < 0.25):
+		drive(0,0)
+		rospy.sleep(.25)
+		if(rlook > llook):
+			while(lfeel < 0.4):
+				drive(2.5,-2.5)
+				rospy.sleep(0.1)
+		else:
+			while(rfeel < 0.4):
+				drive(-2.5,2.5)
+				rospy.sleep(0.1)
